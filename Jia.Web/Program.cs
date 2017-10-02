8447f4a77c24cff863e4c607726a18e9af5855ef
Jeff Lai <JLai@dacgroup.com>
2017-10-02 09:47:24 -0400

    first commit

---
+++ Jia.Web/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Jia.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5500") // default port is 5000
+				.UseWebRoot("wwwroot") // default wwwroot
+				.UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
